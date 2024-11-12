f55ed9eb5fb787b16155b4d1386360785e85b767
Aleena R <aleenarachelmanoj@gmail.com>
2024-11-12 21:27:06 +0530

    Create Aleena Rachel (day1-task1).py

---
+++ Day-1/Aleena Rachel (day1-task1).py
@@ -0,0 +1,9 @@
+"""
+Author: Aleena Rachel
+Date: Nov 12 
+Description: Day 1 ;Task1(Print func) 
+"""
+
+print("Hello, World!")
+print("Welcome to the Python Challenge!") 
+print("Let's make learning Python fun!")
