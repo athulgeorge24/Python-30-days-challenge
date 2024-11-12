fd70fbaf75d1d3e6c539fc3b796f920dccf10e74
Aleena R <aleenarachelmanoj@gmail.com>
2024-11-12 22:24:44 +0530

    Create Aleena Rachel Day1- task2.py

---
+++ Day-1/Aleena Rachel Day1- task2.py
@@ -0,0 +1,8 @@
+"""
+Author: Aleena Rachel
+Date: Nov 12
+Description: Day 1 task 2 (input function) 
+"""
+name=input("Enter your name:") 
+age=input("Enter your age:") 
+print("Your name is",name,"and your age is",age)
