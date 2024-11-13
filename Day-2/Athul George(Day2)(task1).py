b236cfe9bd22edb3b28974e8a4e8f5643b58e41c
athulgeorge24 <athulgeorge2028@cs.sjcetpalai.ac.in>
2024-11-13 23:06:53 +0530

    Add files via upload

---
+++ Day-2/Athul George(Day2)(task1).py
@@ -0,0 +1,20 @@
+# Basic Arithmetic Operations Program
+
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+floor_division = num1 // num2 #(divides two numbers and returns the largest integer)
+exponential = num1 ** num2 #(num1 to the power of num2)
+modulus = num1 % num2 #( returns the remainder of the division)
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
+print("Floor division:",floor_division)
+print("Exponential:",exponential)
+print("Modulus:",modulus)
