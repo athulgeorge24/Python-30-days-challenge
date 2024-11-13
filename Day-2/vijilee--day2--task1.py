4d756c21d7ab4632649e9217a6ef044bf4313765
Vijilee George Kurian <vijileegk2006@gmail.com>
2024-11-13 20:14:04 +0530

    Add files via upload

---
+++ Day-2/vijilee--day2--task1.py
@@ -0,0 +1,16 @@
+number_1=int(input("enter  number1:"))
+number_2=int(input("enter number 2:"))
+sum=number_1+number_2
+print("the sum of number  is:",sum)
+Difference=number_2-number_1
+print("the difference of number is:",Difference)
+product=number_1*number_2
+print("the product of two numbers are:",product)
+integer_div=number_1/number_2
+print("the integer_div of numbers is:",integer_div)
+floor_div=number_1//number_2
+print("the floor of these two is:",floor_div)
+power=number_1**number_2
+print("power is :",power)
+modulus=number_1%number_2
+print("modulus of number is:",modulus)
---
+++ Day-2/vijilee--day2--task2.py
@@ -0,0 +1,4 @@
+name_1=input("enter your first name:")
+name_2=input("enter your last name:")
+full=name_1+" "+name_2
+print("Your name is :",full)
