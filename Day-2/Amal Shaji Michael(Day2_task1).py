7121d59f13071ec1a7b86601955e97dd73fde55e
Amal Shaji Michael <amalshajimichaelk@gmail.com>
2024-11-14 00:38:41 +0530

    Add files via upload

---
+++ Day-2/Amal Shaji Michael(Day2_task1).py
@@ -0,0 +1,18 @@
+num_1=float(input("Enter the first number: "))
+num_2=float(input("Enter the second number: "))
+
+add=num_1+num_2
+subt=num_1-num_2
+multi=num_1*num_2
+div=num_1/num_2
+floor_div=num_1//num_2
+exp=num_1**num_2
+mod=num_1%num_2
+
+print("Sum: ",add)
+print("Difference: ",subt)
+print("Product: ",multi)
+print("Division: ",div)
+print("Floor division: ",floor_div)
+print("Exponential: ",exp)
+print("Modulus: ",mod)
