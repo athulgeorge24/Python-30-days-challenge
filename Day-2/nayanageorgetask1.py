0e7f00262e9a76ac3b2427007569c79b1eb646a6
N1nayanaGeorge <nayanageorge59419@gmail.com>
2024-11-14 07:46:55 +0530

    Update Day-2

---
+++ Day-2/nayanageorgetask1.py
@@ -0,0 +1,16 @@
+x= int(input("Enter a number")) 
+y= int(input("Enter a number"))
+add= x+y
+sub= x-y
+mul=x*y
+exp=x**y
+div=x/y
+f_div=x//y
+mod=x%y
+print(add) 
+print(sub) 
+print(mul) 
+print(exp) 
+print(div) 
+print(f_div) 
+print(mod)
