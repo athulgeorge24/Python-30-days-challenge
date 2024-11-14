3c70fea9ce03e26c5b59cb87f56c950d2a4cd6dc
Vijilee George Kurian <vijileegk2006@gmail.com>
2024-11-14 21:58:33 +0530

    Add files via upload

---
+++ Day-3/vijilee--day3--task1.py
@@ -0,0 +1,7 @@
+"""char_1=input("enter a sentence:")
+word=input("enter a word to check:")
+print("Is",word,"in the sentence",word in char_1)"""
+
+"""a=input("enter a name:")
+b=input("enter a letter:")
+print("wheather",b,"in the name:",b not in a)"""
