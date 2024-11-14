a928f743f90ca3abda0b7f1b95f06194e3d05d80
Amal Shaji Michael <amalshajimichaelk@gmail.com>
2024-11-14 23:51:10 +0530

    Add files via upload

---
+++ Day-3/Amal Shaji Michael(Day3_task1).py
@@ -0,0 +1,7 @@
+name=input("Enter your name: ")
+letter=input("Enter a letter to check: ")
+print("Is the letter",'letter', "in the name?", letter in name)
+
+sent=input("Enter a sentence: ")
+a=input("Enter a letter/word to check:")
+print("Whether",a,"in the sentence: ", a not in sent)
