756f727de70e8be536b54415d3015d06fd4f7e4a
Amal Shaji Michael <amalshajimichaelk@gmail.com>
2024-11-15 00:12:29 +0530

    Add files via upload

---
+++ Day-3/Amal Shaji Michael (Day3_task2).py
@@ -0,0 +1,7 @@
+a=int(input("Enter the 1st number: "))
+b=int(input("Enter the 2nd number: "))
+print("Are a and b are the same?",a is b)
+
+old=int(input("Enter the old number lock: "))
+new=int(input("Enter the new number lock: "))
+print("Aren't the old and new the same: ", old is not new)
