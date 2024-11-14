3bfeb351e9fc6e163b445933c99d9d24b06131f8
Vijilee George Kurian <vijileegk2006@gmail.com>
2024-11-14 22:29:24 +0530

    Update vijilee--day3--task2.py

--- Day-3/vijilee--day3--task2.py
+++ Day-3/vijilee--day3--task2.py
@@ -1,8 +1,8 @@
-"""sen_1=input("enter a sentence:")
+sen_1=input("enter a sentence:")
 word=input("enter a word:")
-print("Is",word,"in the sentence",word is sen_1)"""
+print("Is",word,"in the sentence",word is sen_1)
  
  
-"""char_1=input("enter a name:\t")
+char_1=input("enter a name:\t")
 b=input("enter a word:\t")
-print("Wheather",b,"in the name",b is not char_1)"""
+print("Wheather",b,"in the name",b is not char_1)
