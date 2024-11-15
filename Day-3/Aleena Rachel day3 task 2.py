bba4022d534d74300e0a29e65a17b40ab8f663d5
Aleena R <aleenarachelmanoj@gmail.com>
2024-11-15 12:08:32 +0530

    Create Aleena Rachel day3 task 2.py

---
+++ Day-3/Aleena Rachel day3 task 2.py
@@ -0,0 +1,7 @@
+num_1=int(input("Enter a number: "))
+num_2=int(input("Enter another number: "))
+print("Are first number and second number the same?", num_1 is num_2)
+
+old_password=int(input("Enter the old password"))
+new_password=int(input("Enter the new password"))
+print("Aren't the old and new the same?",old_password is not new_password) 
