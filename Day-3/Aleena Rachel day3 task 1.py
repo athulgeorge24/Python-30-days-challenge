4c6ab0a223d77db3afb3bf486d5eaab17c186413
Aleena R <aleenarachelmanoj@gmail.com>
2024-11-15 11:18:46 +0530

    Update Aleena Rachel day3 task 1.py

--- Day-3/Aleena Rachel day3 task 1.py
+++ Day-3/Aleena Rachel day3 task 1.py
@@ -1,3 +1,7 @@
 word=input("Enter a word")
 letter=input("Enter a letter")
-print("Is letter in the word",letter in word)
+print("Is letter in the word? ",letter in word)
+
+sentence=input("Enter a sentence ") 
+word_check=input(" Enter a word to check in sentence ") 
+print(" Is the word in the sentence? ",word_check not in sentence) 
