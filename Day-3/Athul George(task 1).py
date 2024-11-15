22b8f167a3028b53029ac8073f6805b99e6c13a1
athulgeorge24 <athulgeorge2028@cs.sjcetpalai.ac.in>
2024-11-15 16:52:32 +0530

    Add files via upload

---
+++ Day-3/Athul George(task 1).py
@@ -0,0 +1,7 @@
+sentence=input("enter a sentence:")
+word=input("enter a word to check:")
+print("Is",word,"in the sentence",word in sentence)
+
+first=input("enter a name:")
+second=input("enter a letter:")
+print("wheather",second,"in the name:",second not in first)
