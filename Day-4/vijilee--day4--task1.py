2bb44fdfa709060e07b4059e4320d21a4b01bfd9
Vijilee George Kurian <vijileegk2006@gmail.com>
2024-11-16 10:53:41 +0530

    Add files via upload

---
+++ Day-4/vijilee--day4--task1.py
@@ -0,0 +1,34 @@
+#sqaure root using math module
+import math
+number=int(input("enter a number:"))
+square_root=math.sqrt(number)
+print(f"the square root of number is {square_root}")
+
+#floor of number 
+import math
+num=int(input("enter a number:"))
+floor=math.floor(num)
+print(f"floor of the number is:{floor}")
+
+#power of number's
+import math
+a=int(input("enter a number:"))
+b=int(input("enter the raise to number:"))
+power=math.pow(a,b)
+print(f"power of the number is:{power}")
+
+#factorial of numbers
+import math
+x=int(input("enter a number:"))
+factorial=math.factorial(x)
+print(f"factorial of number is:{factorial}")
+
+#log of number's 
+import math
+p=int(input("enter a number:"))
+q=math.log(p)
+print(f"logarithm of the number is{q}")
+#math.pi,#math.e
+import math
+print(math.pi)
+print(math.e)
