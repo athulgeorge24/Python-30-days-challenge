482f8bea9b10d099aa88970d07eafc04771c7191
Aleena R <aleenarachelmanoj@gmail.com>
2024-11-16 12:32:46 +0530

    Create Aleena Rachel day4.py

---
+++ Day-4/Aleena Rachel day4.py
@@ -0,0 +1,22 @@
+import math
+num= int(input("Enter a number: ")) 
+#square root of number
+result_1= math.sqrt(num) 
+print(" The square root of number is",result_1) 
+#rounds down(largest number Or equal to x) 
+result_2= math.floor(num) 
+print("The floor division of number is ",result_2) 
+#exponential
+num_2=int(input (" Enter the power")) 
+result_3= math.pow(num,num_2) 
+print("The power of number is ",result_3) 
+#factorial of numbers
+factorial=math.factorial(num)
+print("The factorial of number is:", factorial)
+#log of number
+result_4=math.log(num)
+print("The logarithm of the number is",result_4)
+#math.pi
+print(math.pi)
+#math.e
+print(math.e)
