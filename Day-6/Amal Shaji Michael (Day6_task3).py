4549e2138dfed50ef7153f0befb615b75815c5f9
Amal Shaji Michael <183818109+amalshajimichaelk@users.noreply.github.com>
2024-11-18 00:18:13 +0530

    Add files via upload

---
+++ Day-6/Amal Shaji Michael (Day6_task3).py
@@ -0,0 +1,5 @@
+vehicle_speed=float(input("Enter the speed(km/h): "))
+if vehicle_speed>70:
+    print("You are speeding. You will receive a ticket.")
+else:
+    print("You are within the speed limit.")
