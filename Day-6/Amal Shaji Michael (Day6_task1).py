54c7a14ea4101bff5bf0cdc528b474dc0529f747
Amal Shaji Michael <183818109+amalshajimichaelk@users.noreply.github.com>
2024-11-17 23:43:47 +0530

    Add files via upload

---
+++ Day-6/Amal Shaji Michael (Day6_task1).py
@@ -0,0 +1,8 @@
+username="amal"
+password="Amal@1234"
+username_in=input("Enter your username: ")
+password_in=input("Password: ")
+if username==username_in and password==password_in:
+    print("Login successful")
+else:
+    print("Login failed")
