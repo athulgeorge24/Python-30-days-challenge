dc1c640b99c5b9beab8f4d542eb738bab7f9ef18
Vijilee George Kurian <vijileegk2006@gmail.com>
2024-11-18 21:04:49 +0530

    Update vijilee--day7--task1.py

--- Day-7/vijilee--day7--task1.py
+++ Day-7/vijilee--day7--task1.py
@@ -1,5 +1,5 @@
-year=int(input("enter the year:"))
-if year%4==0 or year%400==0:
-    print("it is a leap year:")
-elif year%100 != 0:
-    print("it is not a leap year:")
+year=int(input("Enter the year: "))
+if year%4==0 and year%100!=0 or year%400==0:
+    print("it is Leap year")
+else:
+    print("it is not a Leap year")
