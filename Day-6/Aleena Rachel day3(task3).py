4fa85ff48e56bcecbfab0f52c3aa7109a7b4bca1
Aleena R <aleenarachelmanoj@gmail.com>
2024-11-19 19:40:09 +0530

    Create Aleena Rachel day3(task3).py

---
+++ Day-6/Aleena Rachel day3(task3).py
@@ -0,0 +1,6 @@
+#To check if speed of the vehicle is within the limit 
+user_speed=int(input("Enter the speed(km/hr) : "))
+if user_speed>70 :
+ print("You are speeding. You will receive a ticket.")
+else:
+ print("You are within the speed limit.")
