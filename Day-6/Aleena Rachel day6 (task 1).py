113f34b531b0b1fcb207733c8f27cc7930186408
Aleena R <aleenarachelmanoj@gmail.com>
2024-11-19 19:58:24 +0530

    Create Aleena Rachel day6 (task 1).py

---
+++ Day-6/Aleena Rachel day6 (task 1).py
@@ -0,0 +1,8 @@
+username= "Aleena304"
+password= "Alee@304"
+user_input= input("Enter your usename: ")
+pass_input= input("Enter your password: ")
+if username== user_input and password==pass_input:
+    print("Login successful")
+else:
+    print ("Login failed")
