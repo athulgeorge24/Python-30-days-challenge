f10e21ed272da07854870b97d943716b1081beda
athulgeorge24 <athulgeorge2028@cs.sjcetpalai.ac.in>
2024-11-19 20:08:58 +0530

    Add files via upload

---
+++ Day-7/Athul George (Day 6(task 1)).py
@@ -0,0 +1,8 @@
+a="admin"
+b="password@123"
+username=input("Username:")
+password=input("Password:")
+if username==a and password==b:
+    print("Login is successful")
+else:
+    print("Login failed,Try again")
