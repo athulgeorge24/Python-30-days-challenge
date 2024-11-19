917e714d1e6678b0a5fed013babda0cb4b17fe08
athulgeorge24 <athulgeorge2028@cs.sjcetpalai.ac.in>
2024-11-19 20:10:02 +0530

    temp

--- Day-7/Athul George (Day 6(task 1)).py
+++
@@ -1,8 +0,0 @@
-a="admin"
-b="password@123"
-username=input("Username:")
-password=input("Password:")
-if username==a and password==b:
-    print("Login is successful")
-else:
-    print("Login failed,Try again")
