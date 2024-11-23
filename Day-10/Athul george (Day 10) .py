99ef7d6c702b2266d7143ad0181767e0cee76d68
athulgeorge24 <athulgeorge2028@cs.sjcetpalai.ac.in>
2024-11-23 22:36:40 +0530

    Add files via upload

---
+++ Day-10/Athul george (Day 10) .py
@@ -0,0 +1,7 @@
+food=["Mandi","Al-Faham","Noodles"]
+add_food=input("Enter a food to add:")
+food.append(add_food)
+print("Updated list of Food:",food)
+remove_food=input("Enter a food to remove:")
+food.remove(remove_food)
+print("Final list of food:",food)
