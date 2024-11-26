b07984e1fec29393523ae60c5019b5cdb67e0d33
Vijilee George Kurian <vijileegk2006@gmail.com>
2024-11-26 20:20:40 +0530

    Add files via upload

---
+++ Day-14/vijilee--day14.py
@@ -0,0 +1,13 @@
+#Membership test
+a={1,2,3,4}
+print("whether 2 is in the set ?",2 in a)
+print("Is 5 in the set ?",5 not in a)
+
+#removing elements,using remove()
+num={1,2,3,4,5,6}
+num.remove(7,)
+print(num)
+#using dicard()
+num={1,2,3,4,5,6}
+num.discard(7,)
+print(num)
